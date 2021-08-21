8c300ebc55b0d38b82d9b9aee15f718494bd7695
Feri <87620783+FeriEXP@users.noreply.github.com>
2021-08-21 14:40:36 +0700

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -4,10 +4,10 @@ import asyncio
 from pyrogram import Client, filters, __version__
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from pyrogram.errors import FloodWait
-
 from bot import Bot
 from config import ADMINS, START_MSG, OWNER_ID, CUSTOM_CAPTION, DISABLE_CHANNEL_BUTTON
 from helper_func import subscribed, encode, decode, get_messages
+
 
 @Bot.on_message(filters.command('start') & filters.private & subscribed)
 async def start_command(client: Client, message: Message):
@@ -40,11 +40,11 @@ async def start_command(client: Client, message: Message):
                 ids = [int(int(argument[1]) / abs(client.db_channel.id))]
             except:
                 return
-        temp_msg = await message.reply("Please wait...")
+        temp_msg = await message.reply("Mohon Tunggu...")
         try:
             messages = await get_messages(client, ids)
         except:
-            await message.reply_text("Something went wrong..!")
+            await message.reply_text("Ada yang salah..!")
             return
         await temp_msg.delete()
 
@@ -94,7 +94,7 @@ async def start_command(client: Client, message: Message):
 
 @Bot.on_message(filters.command('start') & filters.private)
 async def not_joined(client: Client, message: Message):
-    text = "<b>You need to join in my Channel/Group to use me\n\nKindly Please join Channel</b>"
+    text = "Anda harus bergabung di Saluran/Grup saya untuk menggunakan saya\n\nMohon bergabung dengan Saluran</b>"
     message_text = message.text
     try:
         command, argument = message_text.split()
